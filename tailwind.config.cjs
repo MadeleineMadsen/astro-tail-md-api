ba5aeeda7282a04e6d5d802a8cce73d3086896a4
Madeleine Madsen <mama0002@stud.kea.dk>
2024-04-23 11:38:58 +0200

    Cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,37 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Prompt"', ...defaultTheme.fontFamily.sans],
 
-                'sans': ['"Prompt"', ...defaultTheme.fontFamily.sans],
-                
-                'display': ['"Roboto"', 'monospace'], // Adds a new `font-display` class
-                
-                },
-            colors: {
-                'persianmosaic': {
-                    100: '#d2dfe2',
-                    200: '#a5c0c5',
-                    300: '#7aa1a9',
-                    400: '#4e838e',
-                    500: '#196673',
-                    600: '#145863',
-                    700: '#104a54',
-                    800: '#072f36',
-                    900: '#02171b',
-                  },
-            }
-        }
+        display: ['"Roboto"', "monospace"], // Adds a new `font-display` class
+      },
+      colors: {
+        persianmosaic: {
+          100: "#d2dfe2",
+          200: "#a5c0c5",
+          300: "#7aa1a9",
+          400: "#4e838e",
+          500: "#196673",
+          600: "#145863",
+          700: "#104a54",
+          800: "#072f36",
+          900: "#02171b",
+        },
+      },
     },
-  };
+  },
+};
